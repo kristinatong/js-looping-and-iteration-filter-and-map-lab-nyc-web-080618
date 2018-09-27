8bff06fb1c63a08567344772d2dbe6398a46b117
kristinatong <kristina.tong@gmail.com>
2018-09-27 15:26:13 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution here:
+const driversWithRevenueOver = (array,num) => {
+  return array.filter(item => item.revenue > num)
+}
+
+const driverNamesWithRevenueOver = (array,num) => {
+  return array.filter(item => item.revenue > num).map(item => item.name)
+}
+
+const exactMatch = (array,obj) => {
+  return array.filter(item => {
+    let matches = false
+    for(i in obj){
+      matches = item[i] === obj[i]
+    }
+    return matches
+  })
+}
+
+const exactMatchToList = (array,obj)=>{
+  return array.filter(item => {
+    let matches = false
+    for(i in obj){
+      matches = item[i] === obj[i]
+    }
+    return matches
+  }).map(item => item.name)
+}
